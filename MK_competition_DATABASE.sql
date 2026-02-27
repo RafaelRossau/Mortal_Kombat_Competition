3e3b9173a1f9a5798eb8fa0f1f5b154b49e8b38c
Rafael Rossau <rafaelrossau@gmail.com>
2026-02-27 11:48:45 -0300

    feat: Adding DATABASE

---
+++ MK_competition_DATABASE.sql
@@ -0,0 +1,9 @@
+create database MK_Competition;
+use MK_Competition;
+
+create table characters(
+ID int auto_increment,
+character_name varchar(50) not null,
+character_type varchar(20) check (character_type in ('fire','water','wind','poison')),
+attack int not null
+);
