925b96ac4defb88cd9410284eba1ba20b20d358c
RafaelRossau <rafaelrossau@gmail.com>
2026-03-03 11:57:07 -0300

    feat: Making the characters appear in the competition screen.

--- MK_competition_DATABASE.sql
+++ MK_competition_DATABASE.sql
@@ -2,7 +2,7 @@ create database MK_Competition;
 use MK_Competition;
 
 create table characters(
-ID int auto_increment,
+ID int auto_increment primary key,
 character_name varchar(50) not null,
 character_type varchar(20) check (character_type in ('fire','water','wind','poison')),
 attack int not null
--- public/mkc.js
+++ public/mkc.js
@@ -1,3 +1,5 @@
+const url = `http://localhost:3000/characters`
+
 function createCharacter(){
     const checked = (idContainer) => {
         const container = document.getElementById(idContainer);
@@ -30,4 +32,94 @@ function createCharacter(){
             body: JSON.stringify({create_name, create_type, create_attack})
         })
     }
+}
+function randomCharacters(){
+    fetch(url)
+    .then(response => response.json())
+    .then (characters => {
+        if (characters.length < 8){
+            alert("8 characters minimum in the DATABASE needed to initiate the competition!")
+            window.location.href = 'http://localhost:3000/mkc_title.html'
+            return;
+        }
+        const randomindex1 = Math.floor(Math.random() * characters.length);
+        const x1 = characters[randomindex1]
+
+        document.getElementById("name1").innerHTML = x1.character_name
+        document.getElementById("type1").innerHTML = x1.character_type
+        document.getElementById("attack1").innerHTML = x1.attack
+
+        const randomindex2 = Math.floor(Math.random() * characters.length);
+        const x2 = characters[randomindex2]
+        if(x2 == x1){
+            return randomCharacters();
+        }
+
+        document.getElementById("name2").innerHTML = x2.character_name
+        document.getElementById("type2").innerHTML = x2.character_type
+        document.getElementById("attack2").innerHTML = x2.attack
+
+        const randomindex3 = Math.floor(Math.random() * characters.length);
+        const x3 = characters[randomindex3]
+        if(x3 == x1 || x3 == x2){
+            return randomCharacters();
+        }
+
+        document.getElementById("name4").innerHTML = x3.character_name
+        document.getElementById("type4").innerHTML = x3.character_type
+        document.getElementById("attack4").innerHTML = x3.attack
+
+        const randomindex4 = Math.floor(Math.random() * characters.length);
+        const x4 = characters[randomindex4]
+        if(x4 == x1 || x4 == x2 || x4 == x3){
+            return randomCharacters();
+        }
+
+        document.getElementById("name5").innerHTML = x4.character_name
+        document.getElementById("type5").innerHTML = x4.character_type
+        document.getElementById("attack5").innerHTML = x4.attack
+
+        const randomindex5 = Math.floor(Math.random() * characters.length);
+        const x5 = characters[randomindex5]
+        if(x5 == x1 || x5 == x2 || x5 == x3 || x5 == x4){
+            return randomCharacters();
+        }
+
+        document.getElementById("name7").innerHTML = x5.character_name
+        document.getElementById("type7").innerHTML = x5.character_type
+        document.getElementById("attack7").innerHTML = x5.attack
+
+        const randomindex6 = Math.floor(Math.random() * characters.length);
+        const x6 = characters[randomindex6]
+        if(x6 == x1 || x6 == x2 || x6 == x3 || x6 == x4 || x6 == x5){
+            return randomCharacters();
+        }
+
+        document.getElementById("name8").innerHTML = x6.character_name
+        document.getElementById("type8").innerHTML = x6.character_type
+        document.getElementById("attack8").innerHTML = x6.attack
+
+        const randomindex7 = Math.floor(Math.random() * characters.length);
+        const x7 = characters[randomindex7]
+        if(x7 == x1 || x7 == x2 || x7 == x3 || x7 == x4 || x7 == x5 || x7 == x6){
+            return randomCharacters();
+        }
+
+        document.getElementById("name9").innerHTML = x7.character_name
+        document.getElementById("type9").innerHTML = x7.character_type
+        document.getElementById("attack9").innerHTML = x7.attack
+
+        const randomindex8 = Math.floor(Math.random() * characters.length);
+        const x8 = characters[randomindex8]
+        if(x8 == x1 || x8 == x2 || x8 == x3 || x8 == x4 || x8 == x5 || x8 == x6 || x8 == x7){
+            return randomCharacters();
+        }
+
+        document.getElementById("name10").innerHTML = x8.character_name
+        document.getElementById("type10").innerHTML = x8.character_type
+        document.getElementById("attack10").innerHTML = x8.attack
+    })
+}
+if (window.location.pathname.includes("mkc.html")) {
+    window.onload = randomCharacters;
 }
